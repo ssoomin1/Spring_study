3cb242b80df7372f132a8ea5cc112ac0ae4341c9
Soomin Shin <ssoomin1@naver.com>
2021-03-24 14:53:41 +0900

    AutoWireDI study

---
+++ AutoWireDI/src/main/java/kr/hs/study/main/MainClass.java
@@ -0,0 +1,16 @@
+package kr.hs.study.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("kr/hs/study/config/config.xml");
+		
+		
+		System.out.println();
+	
+	}
+
+}
